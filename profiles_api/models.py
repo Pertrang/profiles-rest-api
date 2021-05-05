3e27c19940dcfb9feee9bb4519e5fc8d77b91722
Per Trangbæk <pertrang+git@gmail.com>
2021-05-05 19:40:32 +0200

    enable user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,27 +8,27 @@ class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
     def create_user(self, email, name, password=None):
-        """ create a new user profile """
+        """Create a new user profile"""
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user =  self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, anme, password):
-        """create a new super user """
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
-        
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
-        return user    
+        return user 
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
@@ -41,16 +41,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUERED_FIELDS = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """Retrive full name of user"""
+        """Retrieve full name for user"""
         return self.name
 
     def get_short_name(self):
-        """return short name of user"""
+        """Retrieve short name of user"""
         return self.name
 
     def __str__(self):
-        """return string representation of our user"""
+        """Return string representation of user"""
         return self.email
